a6248179960c10f3b3278c4f4ef9d8e4030cd571
Aljotennis <aljotennis9747@gmail.com>
2026-08-05 12:05:35 +0530

    Added Java programs

---
+++ pattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class pattern {
+
+	public static void main(String[] args) {
+
+	Scanner s = new Scanner(System.in);
+
+	System.out.print("Enter the number of rows: ");
+
+	int a = s.nextInt();
+
+	for (int i = 1; i <= a ; i ++) {
+
+		for (int j = 1; j <= i; j ++) {
+
+			System.out.print("*");
+
+			}
+		System.out.println();
+
+		}
+
+
+	}
+
+}
